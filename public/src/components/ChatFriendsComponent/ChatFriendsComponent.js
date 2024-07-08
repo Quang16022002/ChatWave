a07ce407b4827d8101476dfab8980530a4c259b9
Nguyen Dinh Quang <20010818@st.phenikaa-uni.edu.vn>
2024-07-08 10:33:27 +0700

    chat

--- public/src/components/ChatFriendsComponent/ChatFriendsComponent.js
+++ public/src/components/ChatFriendsComponent/ChatFriendsComponent.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatFriendsComponent.scss";
+
+const messages = [
+  { fromSelf: false, text: "Bạn khỏe không?", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: false, text: "Nhớ ngày mai có lịch họp lớp nhé", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Địa điểm ở đâu vậy?" },
+  { fromSelf: true, text: "Xa không" },
+  { fromSelf: false, text: "HighLand Coffee", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Ok bạn, rất vui được gặp mọi người." },
+  { fromSelf: false, text: "Mình sẽ có mặt đúng giờ.", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Tuyệt vời! Hẹn gặp lại!" },
+  { fromSelf: false, text: "Okie, hẹn gặp lại.", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Chúng ta có những hoạt động gì" },
+  { fromSelf: false, text: "Vui chơi, karaoke", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Xa không" },
+  { fromSelf: false, text: "HighLand Coffee", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Ok bạn, rất vui được gặp mọi người." },
+  { fromSelf: false, text: "Mình sẽ có mặt đúng giờ.", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Tuyệt vời! Hẹn gặp lại!" },
+  { fromSelf: false, text: "Okie, hẹn gặp lại.", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+  { fromSelf: true, text: "Chúng ta có những hoạt động gì" },
+  { fromSelf: false, text: "Vui chơi, karaoke", avatar: "https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" },
+];
+
 const ChatFriendsComponent = () => {
+  const [inputValue, setInputValue] = useState('')
+  const handleInputChang = (e) => {
+      setInputValue(e.target.value)
+  }
   return (
     <div className="ChatFriendsComponent">
       <div className="row">
         <div className="ChatFriendsComponent-item col-md-4 col-sm-6 px-0">
           <div className="ChatFriendsComponent-item-header">
-            <div className=" d-flex align-items-center ">
+            <div className="d-flex align-items-center">
               <img
                 style={{ marginLeft: 10 }}
                 className="avatar-homeAll chatfriends-img"
                 src="https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg"
+                alt="friend-avatar"
               />
-              <div
-                style={{ width: "100%" }}
-                className="d-flex justify-content-between"
-              >
-                <div className="infoUser d-flex flex-column ">
-                  <p
-                    style={{
-                      color: "rgb(255, 18, 89)",
-                      fontWeight: 500,
-                      marginBottom: 0,
-                    }}
-                  >
-                    Online
-                  </p>
+              <div style={{ width: "100%" }} className="d-flex justify-content-between">
+                <div className="infoUser d-flex flex-column">
+                  <p style={{ color: "rgb(255, 18, 89)", fontWeight: 500, marginBottom: 0 }}>Online</p>
                   <h1 style={{ fontSize: 16 }}>Nguyễn Đình Quang</h1>
                 </div>
                 <div className="d-flex align-items-center ChatFriendsComponent-item-header-right">
-                  <i class="fa-regular fa-bookmark"></i>
-                  <i class="fa-solid fa-magnifying-glass"></i>
-                  <i class="fa-solid fa-ellipsis-vertical"></i>
+                  <i className="fa-regular fa-bookmark"></i>
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                  <i className="fa-solid fa-ellipsis-vertical"></i>
                 </div>
               </div>
             </div>
@@ -39,92 +56,50 @@ const ChatFriendsComponent = () => {
           <div className="ChatFriendsComponent-item-body">
             <div className="ChatFriendsComponent-time">02/06/2024</div>
             <div className="ChatFriendsComponent-content">
-              <div className="ChatFriendsComponent-content-chat-item">
-                <img src="https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" />
-                <p>Bạn khỏa không</p>
-              </div>
-              <div className="ChatFriendsComponent-content-chat-item">
-                <img src="https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" />
-                <p>
-                  Nhớ ngày mai có lịch <br></br>họp lớp nhé
-                </p>
-              </div>
-
-              <div className="ChatFriendsComponent-content-chat-item-me">
-                <p>Tôi khỏe, địa điểm ở đâu vậy</p>
-              </div>
-            <div className="ChatFriendsComponent-content-chat-item">
-              <img src="https://i.pinimg.com/736x/24/21/85/242185eaef43192fc3f9646932fe3b46.jpg" />
-              <p>HighLand coffe</p>
-            </div>
+              {messages.map((message, index) => (
+                <div key={index} className={`ChatFriendsComponent-content-chat-item${message.fromSelf ? "-me" : ""} py-2`}>
+                  {!message.fromSelf && (
+                    <img
+                      src={message.avatar}
+                      alt="chat-avatar"
+                    />
+                  )}
+                  <p>{message.text}</p>
+                </div>
+              ))}
             </div>
           </div>
-
-          <div className="ChatFriendsComponent-item-bottom">đaaf</div>
-        </div>
-        <div className="ChatFriendsComponent-item col-md-4 col-sm-6 px-0">
-          <div className="ChatFriendsComponent-item-header">
-            <div className=" d-flex align-items-center ">
-              <img
-                className="avatar-homeAll chatfriends-img"
-                src="https://cellphones.com.vn/sforum/wp-content/uploads/2024/02/anh-avatar-cute-95.jpg"
-              />
-              <div
-                style={{ width: "100%" }}
-                className="d-flex justify-content-between"
-              >
-                <div className="infoUser d-flex flex-column ">
-                  <p
-                    style={{
-                      color: "rgb(148, 148, 148)",
-                      fontWeight: 500,
-                      marginBottom: 0,
-                    }}
-                  >
-                    Offline
-                  </p>
-                  <h1 style={{ fontSize: 16 }}>Nguyễn Tuân</h1>
-                </div>
-                <div className="d-flex align-items-center ChatFriendsComponent-item-header-right">
-                  <i class="fa-regular fa-bookmark"></i>
-                  <i class="fa-solid fa-magnifying-glass"></i>
-                  <i class="fa-solid fa-ellipsis-vertical"></i>
-                </div>
+          <div className="ChatFriendsComponent-item-bottom">
+            <div className="ChatFriendsComponent-item-bottom-icon d-flex">
+              <div className="ChatFriendsComponent-item-bottom-icon-left">
+                <i className="fa-solid fa-microphone"></i>
+                <i className="fa-regular fa-image"></i>
+                <i className="fa-solid fa-note-sticky"></i>
+                <i className="fa-solid fa-gift"></i>
+              </div>
+              <div className="ChatFriendsComponent-item-bottom-icon-right"> 
+                <i className="fa-solid fa-robot"></i>
+                <span>Tôi có thể gợi ý câu trả lời cho bạn...</span>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="ChatFriendsComponent-item col-md-4 col-sm-6 px-0">
-          <div className="ChatFriendsComponent-item-header">
-            <div className=" d-flex align-items-center ">
-              <img
-                className="avatar-homeAll chatfriends-img"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpYwyHQmE53Lfk8lPBtwL4CGA80jpbKVESEI84g2QktFGxJvgnWKlFbt1N3s0gzaEykKY&usqp=CAU"
-              />
-              <div
-                style={{ width: "100%" }}
-                className="d-flex justify-content-between"
-              >
-                <div className="infoUser d-flex flex-column ">
-                  <p
-                    style={{
-                      color: "rgb(148, 148, 148)",
-                      fontWeight: 500,
-                      marginBottom: 0,
-                    }}
-                  >
-                    Offline
-                  </p>
-                  <h1 style={{ fontSize: 16 }}>văn Thịnh</h1>
-                </div>
-                <div
-                  style={{ marginRight: 10 }}
-                  className="d-flex align-items-center ChatFriendsComponent-item-header-right"
-                >
-                  <i class="fa-regular fa-bookmark"></i>
-                  <i class="fa-solid fa-magnifying-glass"></i>
-                  <i class="fa-solid fa-ellipsis-vertical"></i>
-                </div>
+            <div className="ChatFriendsComponent-item-bottom-message d-flex align-items-center">
+              <div className="ChatFriendsComponent-item-bottom-message-left">
+                <input
+                value={inputValue}
+                onChange={handleInputChang}
+                placeholder="Nhập tin nhắn..." />
+                <i className="fa-solid fa-face-smile"></i>
+              </div>
+              <div className="ChatFriendsComponent-item-bottom-message-right">
+               {
+                inputValue ? (
+
+                  <i class="fa-solid fa-paper-plane"></i>
+                ): (
+                  <i className="fa-solid fa-thumbs-up"></i>
+
+                )
+               }
               </div>
             </div>
           </div>
